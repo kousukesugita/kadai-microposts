231f73ccc28efb9d85e5bb83cc0b67ffe5eccad5
kousukesugita <a3tst6ye@mx1.kcv.ne.jp>
2017-05-17 07:07:01 +0000

    redirect_to user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,6 +5,7 @@ class RelationshipsController < ApplicationController
     user = User.find(params[:follow_id])
     current_user.follow(user)
     flash[:success] = 'ユーザをフォローしました。'
+    redirect_to user
   end
 
   def destroy
